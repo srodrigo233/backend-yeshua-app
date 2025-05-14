84fbcfae5373fbf6f88f51b361754e331df680ee
srodrigo233 <sistemas.shema.OP1@gmail.com>
2025-05-14 15:53:49 -0400

    initial commit

---
+++ src/cats/cats.module.ts
@@ -0,0 +1,14 @@
+import { Global, Module } from "@nestjs/common";
+import { CatsController } from "./cats.controller";
+import { CatsService } from "./cats.service";
+
+@Global()
+@Module({
+    imports: [],
+    controllers: [CatsController],
+    providers: [CatsService],
+    exports: [CatsService],
+})
+export class CatsModule {
+    constructor(private catsService: CatsService) {}
+}
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { Cat } from './interfaces/cat.interface';
+
+@Injectable()
+export class CatsService {
+    private readonly cats: Cat[] = [ ];
+    
+    create(cat: Cat) {
+        this.cats.push(cat);
+    }
+
+    findAll():Cat[] {
+        return this.cats;
+    }
+
+    // findOne(id: number) {
+    //     return this.cats.find(cat => cat.id === id);
+    // }
+}
+
